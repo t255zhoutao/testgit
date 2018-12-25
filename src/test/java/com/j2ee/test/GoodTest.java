19e73b4ce21fe9f33c44a8e2378482297147c48e
Zhoutao <958318249@qq.com>
2018-12-25 13:59:33 +0800

    commit test2

--- src/test/java/com/j2ee/test/GoodTest.java
+++ src/test/java/com/j2ee/test/GoodTest.java
@@ -1,5 +1,9 @@
 package com.j2ee.test;
 
 public class GoodTest {
-
+	public void helloTest(){
+		System.out.println("hello test");
+		System.out.println("hello test1");
+		System.out.println("hello test2");
+	}
 }
